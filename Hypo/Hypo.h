dbeacf5a65e91cd5d9d804c375033af82f583c7f
rentzsch <jwr.git@redshed.net>
2014-03-22 22:16:50 -0500

    Move away from 2.x option dictionary approach to typesafe block approach. Also allow setup to be called directly in case you can't control the alloc init.

--- Hypo/Hypo.h
+++ Hypo/Hypo.h
@@ -1,24 +1,26 @@
-// Hypo.h semver:2.0b1
-//   Copyright (c) 2010-2013 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
+// Hypo.h semver:3.0b1
+//   Copyright (c) 2010-2014 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
 //   Some rights reserved: http://opensource.org/licenses/mit
 //   http://github.com/rentzsch/hypo
 
 #import <Foundation/Foundation.h>
 
+typedef void (^HypoBlock)(id instance);
+
 @interface NSObject (hypo_new)
 + (instancetype)hypo_new;
-+ (instancetype)hypo_new:(NSDictionary*)opts;
++ (instancetype)hypo_new:(HypoBlock)block;
+
+// For when you can't call +hypo_new directly (f.x., instantiation from nibs):
+- (void)hypo_setup;
 @end
 
 @interface HypoClass : NSObject
 - (id)hypo_new;
-- (id)hypo_new:(NSDictionary*)opts;
+- (id)hypo_new:(HypoBlock)block;
 @end
 
-extern NSString const * HypoCallback;
-
-typedef id (^HypoCallbackBlock)(
-    id instance,
-    NSString *propertyName,
-    NSString *propertyClassName,
-    NSDictionary *opts);
+@interface NSObject (hypo_awakeFromNew)
+// Overriders should call [super hypo_awakeFromNew].
+- (void)hypo_awakeFromNew;
+@end
--- Hypo/Hypo.m
+++ Hypo/Hypo.m
@@ -1,19 +1,15 @@
-// Hypo.m semver:2.0b1
-//   Copyright (c) 2010-2013 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
+// Hypo.m semver:3.0b1
+//   Copyright (c) 2010-2014 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
 //   Some rights reserved: http://opensource.org/licenses/mit
 //   http://github.com/rentzsch/hypo
 
 #import "Hypo.h"
 #import <objc/runtime.h>
 
-#if __has_feature(objc_arc)
-    #define autorelease self
-#endif
-
 @implementation NSObject (hypo_new)
 
 + (instancetype)hypo_new {
-    return [self hypo_new:@{}];
+    return [self hypo_new:NULL];
 }
 
 static NSString *classNameFromPropertyAttributes(const char *attributesCStr) {
@@ -29,79 +25,66 @@ static NSString *classNameFromPropertyAttributes(const char *attributesCStr) {
     NSRange classNameRange = NSMakeRange(openingQuote.location + 1,
                                          closingQuote.location - openingQuote.location - 1);
     NSString *result = [attributes substringWithRange:classNameRange];
-    NSCAssert(result && [result length] >= 3, nil);
+    NSCAssert1(result && [result length] >= 3,
+               @"Hypo: couldn't extract class name from property attribute string '%s'",
+               attributesCStr);
     return result;
 }
 
-+ (instancetype)hypo_new:(NSDictionary*)opts {
++ (instancetype)hypo_new:(HypoBlock)block {
     id instance = [[self alloc] init];
     
-    Class currentClass = [instance class];
+    if (block) {
+        block(self);
+    }
+    
+    [instance hypo_setup];
+    
+#if !__has_feature(objc_arc)
+    [instance autorelease];
+#endif
+    return instance;
+}
+
+- (void)hypo_setup {
+    Class currentClass = [self class];
     do {
         unsigned propertyCount;
         objc_property_t *properties = class_copyPropertyList(currentClass, &propertyCount);
         for (unsigned propertyIndex = 0; propertyIndex < propertyCount; propertyIndex++) {
             objc_property_t property = properties[propertyIndex];
             NSString *propertyName = [NSString stringWithUTF8String:property_getName(property)];
-            NSString *propertyClassName = classNameFromPropertyAttributes(property_getAttributes(property));
-            if (!propertyClassName) continue;
             
-            if ([propertyName hasSuffix:@"_hypo"]) {
-                // Instance injection request.
-                if (![instance valueForKey:propertyName]) {
-                    Class propertyClass = NSClassFromString(propertyClassName);
-                    NSAssert1(propertyClass, @"Hypo: couldn't find class named \"%@\"", propertyClassName);
-                    
-                    // First try assigning an existing instance.
-                    id propertyValue = opts[propertyName];
-                    
-                    // If that failed second try the callback mechanism.
-                    if (!propertyValue) {
-                        HypoCallbackBlock callback = opts[HypoCallback];
-                        if (callback) {
-                            propertyValue = callback(instance,
-                                                     propertyName,
-                                                     propertyClassName,
-                                                     opts);
-                        }
-                    }
-                    
-                    // Finally if all those failed just create a new instance ourself.
-                    if (!propertyValue) {
-                        NSString *keypathPrefix = [propertyName stringByAppendingString:@"."];
-                        NSMutableDictionary *subOpts = [NSMutableDictionary dictionary];
-                        for (NSString *key in opts) {
-                            if ([key hasPrefix:keypathPrefix]) {
-                                NSString *subkeypath = [key substringFromIndex:[keypathPrefix length]];
-                                subOpts[subkeypath] = opts[key];
-                            } else {
-                                subOpts[key] = opts[key];
-                            }
-                        }
-                        propertyValue = [propertyClass hypo_new:subOpts];
-                    }
-                    
-                    [instance setValue:propertyValue forKey:propertyName];
+            if ([self valueForKey:propertyName] == nil) {
+                NSString *propertyClassName = classNameFromPropertyAttributes(property_getAttributes(property));
+                
+                Class propertyClass = NSClassFromString(propertyClassName);
+                NSAssert1(propertyClass, @"Hypo: couldn't find class named \"%@\"", propertyClassName);
+                
+                if ([propertyName hasSuffix:@"_hypo"]) {
+                    // Unfulfilled instance injection request.
+                    [self setValue:[propertyClass hypo_new] forKey:propertyName];
+                } else if ([propertyClassName isEqualToString:@"HypoClass"]) {
+                    //  Unfulfilled class injection request.
+                    [self setValue:propertyClass forKey:propertyName];
                 }
-            } else if ([propertyClassName isEqualToString:@"HypoClass"]) {
-                //  Class injection request.
-                Class propertyClass = NSClassFromString(propertyName);
-                [instance setValue:propertyClass forKey:propertyName];
             }
         }
         free(properties);
         currentClass = class_getSuperclass(currentClass);
     } while (currentClass && currentClass != [NSObject class]);
     
-    return [instance autorelease];
+    [self hypo_awakeFromNew];
 }
 
 @end
 
 @implementation HypoClass
-// These are never actually called:
-- (id)hypo_new { return nil; }
-- (id)hypo_new:(NSDictionary*)opts { return nil; }
+// These are never actually called and only exist to satisfy the linker:
+- (id)hypo_new { assert(0); return nil; }
+- (id)hypo_new:(HypoBlock)block { assert(0); return nil; }
 @end
 
-NSString const * HypoCallback = @":HypoCallback";
+@implementation NSObject (hypo_awakeFromNew)
+- (void)hypo_awakeFromNew {} // Default do-nothing implementation. 
+@end
